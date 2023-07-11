8fc068e64039bf1ce127e8a2588862097fe0c62e
daniloschiavi <daniloschiavi39@gmail.com>
2023-07-10 23:59:55 -0300

    Adiciona função soma

--- Calculadora.rb
+++ Calculadora.rb
@@ -1,3 +1,18 @@
 puts "--------------------------"
 puts "Bem bindo a calculadora! |"
 puts "--------------------------"
+puts ""
+puts "Seleciona a opção desejada"
+puts "1. Adição"
+puts "2. Subtração"
+
+op = gets.chomp
+case op
+when "1"
+    puts "Numero A:"
+    a = gets.chomp
+    puts "Numero B:"
+    b = gets.chomp
+    puts "A soma de #{a} e #{b} é: #{a.to_i+b.to_i}"    
+else "opção inválida"
+end
